4031b823bc663f1b5d463bef50bf68c9742c8aa8
TatsutaScott <tatsuta.scott@gmail.com>
2023-03-28 12:38:28 +0900

    adjusted title section to display new json format

--- src/components/title_section.vue
+++ src/components/title_section.vue
@@ -1,89 +1,120 @@
 <template>
-    <div id="titleSection">
-        <h1>{{ props.title }}</h1>
-        <div id="infoSection">
-            <div class="info">
-                <span class="descriptor">Source: </span>
-                <a v-if="isURL(props.source)" :href="props.source">link</a>
-                <span v-else>{{ props.source }}</span>
-            </div>
-            <div class="info">
-                <span class="descriptor">Time: </span>
-                <span>{{ props.time }}</span>
-            </div>
-            <div class="info">
-                <span class="descriptor">Servings: </span>
-                <span>{{ props.servings }}</span>
-            </div>
-            <div class="info">
-                <span class="descriptor">Tags: </span>
-                <span class="tag" v-for="tag in props.tags" :key="tag"> {{ tag }}</span>
-            </div>
-        </div>   
+  <div id="titleSection">
+    <h1>{{ props.title }}</h1>
+    <div id="infoSection">
+      <div class="info">
+        <span class="descriptor">Source: </span>
+        <a v-if="isURL(props.source)" :href="props.source">link</a>
+        <span v-else>{{ props.source }}</span>
+      </div>
+      <div class="info">
+        <span class="descriptor">Time: </span>
+        <span>{{ `${props.time.amount} ${props.time.unit}` }}</span>
+      </div>
+      <div class="info">
+        <span class="descriptor">Makes: </span>
+        <span>{{ `${props.servings.amount} ${props.servings.unit}` }}</span>
+      </div>
+      <div class="info">
+        <span class="descriptor">Date: </span>
+        <span>{{ `${dateToString(props.date)}` }}</span>
+      </div>
+      <div class="info">
+        <span class="descriptor">Tags: </span>
+        <span class="tag" v-for="tag in props.tags" :key="tag"> {{ tag }}</span>
+      </div>
     </div>
-
+  </div>
 </template>
 
 <script setup>
-import { defineProps } from 'vue'
+import { defineProps } from "vue";
 
 const props = defineProps({
-    title: String, 
-    source: String, 
-    time: Number, 
-    servings: Number, 
-    tags: Array
+  title: String,
+  source: String,
+  time: Object,
+  servings: Object,
+  date: Array,
+  tags: Array,
 });
 
 function isURL(text) {
-    return text.slice(0, 4) == 'http';
+  return text.slice(0, 4) == "http";
+}
+
+function monthToString(monthNumber) {
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  if (monthNumber < 1 || monthNumber > 12) {
+    throw new Error("Invalid month number.");
+  }
+
+  return monthNames[monthNumber - 1];
+}
+
+function dateToString(dateArray) {
+  return `${monthToString(dateArray[1])} ${dateArray[0]}, ${dateArray[2]}`;
 }
 </script>
 
 <style lang="scss">
-@import '../assets/styles/globalStyles.scss';
-#title{
-    @include container;
-    @include flex;
-    
-    // padding: auto 2rem !important;
+@import "../assets/styles/globalStyles.scss";
+#title {
+  @include container;
+  @include flex;
+
+  // padding: auto 2rem !important;
 }
-#titleSection{
-    // margin: auto 2rem;
-    @include basic;
-    @include flex(row, flex-start, baseline);
+#titleSection {
+  // margin: auto 2rem;
+  @include basic;
+  @include flex(row, flex-start, baseline);
 }
 #infoSection {
-    @include flex(row);
-    margin-left: 3em;
+  @include flex(row);
+  margin-left: 3em;
 }
 h1 {
-    font-size: 3em;
-    margin: 0;
+  font-size: 3em;
+  margin: 0;
 }
 .tag {
-        font-size: 0.75em;
-        padding: 0.125em 0.5em;
-        margin-right: 0.5em;
-        border: 1px solid $color-weak;
-        border-radius: 5px;
-        background-color: $color-weak;
-        color: white;
-        transition: background-color 0.25s ease-out 50ms
+  font-size: 0.75em;
+  padding: 0.125em 0.5em;
+  margin-right: 0.5em;
+  border: 1px solid $color-weak;
+  border-radius: 5px;
+  background-color: $color-weak;
+  color: white;
+  transition: background-color 0.25s ease-out 50ms;
 }
-.tag:hover{
-        background-color: black;
-        border: 1px solid black;
+.tag:hover {
+  background-color: black;
+  border: 1px solid black;
 }
 
-.info{
-    margin-right: 0.5em;
-    display: flex;
-    align-items: center;
+.info {
+  margin-right: 1em;
+  display: flex;
+  align-items: center;
 }
-.descriptor{
-    color: $color-weak;
-    font-size: 1em;
-    margin-right: 0.25em;
+.descriptor {
+  color: $color-weak;
+  font-size: 1em;
+  margin-right: 0.25em;
 }
 </style>
--- src/pages/recipe-page.vue
+++ src/pages/recipe-page.vue
@@ -1,39 +1,57 @@
 <template>
   <div id="recipe">
     <div class="title">
-      <titleSection v-if="recipe" id="title" :title="recipe.title" :source="recipe.source" :time="recipe.time" :servings="recipe.servings" :tags="recipe.tags" />
+      <titleSection
+        v-if="recipe"
+        id="title"
+        :title="recipe.title"
+        :source="recipe.source"
+        :time="recipe.time"
+        :servings="recipe.servings"
+        :tags="recipe.tags"
+        :date="recipe.date"
+      />
     </div>
+
     <div class="details">
-      <ingredientsSection v-if="recipe" id="ingredients" :ingredients="recipe.ingredients" />
-      <directionsSection v-if="recipe" id="directions" :directions="recipe.directions" />
+      <ingredientsSection
+        v-if="recipe"
+        id="ingredients"
+        :ingredients="recipe.ingredients"
+      />
+
+      <directionsSection
+        v-if="recipe"
+        id="directions"
+        :directions="recipe.directions"
+      />
     </div>
   </div>
 </template>
 
 <script setup>
-import { defineProps, ref } from 'vue';
+import { defineProps, ref } from "vue";
 
-import titleSection from '../components/title_section.vue'
-import ingredientsSection from '../components/ingredients_section.vue'
-import directionsSection from '../components/directions_section.vue'
+import titleSection from "../components/title_section.vue";
+import ingredientsSection from "../components/ingredients_section.vue";
+import directionsSection from "../components/directions_section.vue";
 
 const props = defineProps({
-  name: String
-}); 
+  name: String,
+});
 
 const recipe = ref(null);
-import(`../static/${props.name}.json`)
-  .then(data =>recipe.value = data);
+import(`../static/${props.name}.json`).then((data) => (recipe.value = data));
 </script>
 
 <style lang="scss">
-@import '@/assets/styles/globalStyles.scss';
+@import "@/assets/styles/globalStyles.scss";
 
-#recipe{
-  display: flex; 
+#recipe {
+  display: flex;
   flex-direction: column;
 }
-.details{
+.details {
   display: flex;
   flex-direction: row;
 }
